a75828ff3ad42055ffe9efdb17006fe0c1180fa4
nicolaseron <nicoco@live.be>
2024-11-20 18:34:35 +0100

    resolve candidat repo

--- src/main/java/be/ifapme/election/repository/CandidatRepository.java
+++ src/main/java/be/ifapme/election/repository/CandidatRepository.java
@@ -1,12 +1,13 @@
 package be.ifapme.election.repository;
 
 import be.ifapme.election.model.Candidat;
+import be.ifapme.election.model.CandidatId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CandidatRepository extends JpaRepository<Candidat, Integer> {
+public interface CandidatRepository extends JpaRepository<Candidat, CandidatId> {
     List<Candidat> findAllByElection_Id (Integer electionId);
 }
