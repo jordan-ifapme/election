b0930fcfcf383bce2232c814c06e39c2fd7b38f3
Virusf <48204427+Virusf@users.noreply.github.com>
2024-12-11 19:22:52 +0100

    Modif Sql

---
+++ src/main/java/be/ifapme/election/sql/V06/Modif_sql_postgre.sql
@@ -0,0 +1,59 @@
+-- 1. Modification de colonnes dans `adresse`
+ALTER TABLE adresse
+ALTER COLUMN localite TYPE VARCHAR(255),
+ALTER COLUMN code_postal TYPE VARCHAR(10),
+ALTER COLUMN rue TYPE VARCHAR(255),
+ALTER COLUMN boite TYPE VARCHAR(255);
+
+-- 2. Suppression et ajout d'une clé étrangère dans `candidat`
+ALTER TABLE election.candidat DROP CONSTRAINT IF EXISTS fk_candidat_partit;
+
+ALTER TABLE election.candidat
+ADD CONSTRAINT fk_candidat_partit
+FOREIGN KEY (partit_id)
+REFERENCES election.partit(id)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
+
+-- 3. Modification de colonnes dans `candidat`
+ALTER TABLE candidat
+ALTER COLUMN partit_id TYPE BIGINT,
+ALTER COLUMN vote TYPE INT SET DEFAULT 0;
+
+-- 4. Suppression et ajout d'une clé étrangère dans `election`
+ALTER TABLE election.election DROP CONSTRAINT IF EXISTS fk_code_pays_election;
+
+ALTER TABLE election.election
+ADD CONSTRAINT fk_code_pays_election
+FOREIGN KEY (code_pays)
+REFERENCES election.pays(code_pays)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
+
+-- 5. Modification de colonnes dans `election`
+ALTER TABLE election
+ALTER COLUMN nom TYPE VARCHAR(255),
+ALTER COLUMN date_limite TYPE TIMESTAMP SET DEFAULT CURRENT_TIMESTAMP,
+ALTER COLUMN code_pays TYPE VARCHAR(255);
+
+-- 6. Modification de colonnes dans `partit`
+ALTER TABLE partit
+ALTER COLUMN nom TYPE VARCHAR(255),
+ALTER COLUMN couleur TYPE VARCHAR(50);
+
+-- 7. Suppression et ajout d'une clé étrangère dans `personne`
+ALTER TABLE election.personne DROP CONSTRAINT IF EXISTS fk_personne_adresse;
+
+ALTER TABLE election.personne
+ADD CONSTRAINT fk_personne_adresse
+FOREIGN KEY (adresse_id)
+REFERENCES election.adresse(id)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
+
+-- 8. Modification de colonnes dans `personne`
+ALTER TABLE personne
+ALTER COLUMN nom TYPE VARCHAR(255),
+ALTER COLUMN prenom TYPE VARCHAR(255),
+ALTER COLUMN registre_national TYPE VARCHAR(20),
+ALTER COLUMN adresse_id TYPE BIGINT;
--- src/main/java/be/ifapme/election/sql/v04/Create_pays_postgres.sql
+++ src/main/java/be/ifapme/election/sql/v04/Create_pays_postgres.sql
@@ -4,6 +4,20 @@ CREATE TABLE IF NOT EXISTS public.pays
     nom character varying COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT pays_pkey PRIMARY KEY (code_pays)
     );
+
+ALTER TABLE adresse
+ADD COLUMN code_pays VARCHAR(20) NOT NULL;
+
+INSERT INTO pays (code_pays, nom) VALUES ('BE', 'Belgique');
+
+UPDATE adresse
+SET code_pays = 'BE'
+WHERE code_pays = '';
+
+UPDATE election
+SET code_pays = 'BE'
+WHERE code_pays = '';
+
 ALTER TABLE IF EXISTS public.adresse
     ADD FOREIGN KEY (code_pays)
     REFERENCES public.pays (code_pays) MATCH SIMPLE
